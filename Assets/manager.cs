3436c102fdcb4d37aa86eef8fb3065a7f24751ed
Grajfo <zan.grajfoner.1996@gmail.com>
2019-04-02 12:08:40 +0200

    Gui_Update_main

---
+++ Assets/manager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class manager : MonoBehaviour
+{
+    public void scena(int scena)
+    {
+        SceneManager.LoadScene(scena);
+    }
+}
