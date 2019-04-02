c383bf9ceaaaba9cd520a7fc151f70689af3bdb0
Grajfo <zan.grajfoner.1996@gmail.com>
2019-04-02 11:27:59 +0200

    maingui

---
+++ Assets/MAINGUI/Resulution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resulution : MonoBehaviour
+{
+
+  //  public int Width;
+   // public int Height;
+
+    void Start()
+    {
+        Screen.SetResolution(600, 900, false);
+    }
+
+   /* public void width(int w)
+    {
+        Width = w;
+    }
+
+
+    public void setHeight(int h)
+    {
+        Height = h;
+    }
+
+    public void setResolution()
+    {
+        Screen.SetResolution(Width, Height, false);
+    }*/
+}
