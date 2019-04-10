e5691434897a503b4bbff3f580db4abe44e7fd73
Grajfo <zan.grajfoner.1996@gmail.com>
2019-04-10 20:53:08 +0200

    update

---
+++ Assets/Endless Runner/script/ReapitingBg.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReapitingBg : MonoBehaviour
+{
+    public float speed;
+
+    public float EndX;
+    public float startX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if(transform.position.x <= EndX)
+        {
+            transform.position = new Vector2(startX, transform.position.y);
+
+        }
+    }
+}
